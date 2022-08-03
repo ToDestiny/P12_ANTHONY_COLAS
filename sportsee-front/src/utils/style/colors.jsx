752258401619fba5231f12a7775be72b73d05975
Anthony <anthonycolas.france@gmail.com>
2022-08-03 16:35:37 +0200

    Added - Colors file

---
+++ sportsee-front/src/utils/style/colors.jsx
@@ -0,0 +1,14 @@
+const colors = {
+  primary: '#FF6060',
+  secondary: '#F7F7F7',
+  third: '#030303',
+
+  text: '#FF6060',
+  background: '#F7F7F7;',
+  white: '#FDFDFD',
+  black: '#000000',
+  purple: '#122A39',
+  red: '#E11A10',
+};
+
+export default colors;
