91c6bb9d2fb61997ab8ee55e3af1ba3ebf719732
Anthony <anthonycolas.france@gmail.com>
2022-08-03 17:49:17 +0200

    Feature Dashboard - SideBar OK

---
+++ sportsee-front/src/components/LeftBar/LeftBar.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+
+import YogaIcon from '../../assets/icons/icon1.svg';
+import SwimmingIcon from '../../assets/icons/icon2.svg';
+import BikingIcon from '../../assets/icons/icon3.svg';
+import LiftingIcon from '../../assets/icons/icon4.svg';
+
+const NavContainer = styled.header`
+  height: 100vh;
+  width: 5em;
+  margin-top: -4em;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.black};
+`;
+
+const NavBar = styled.nav`
+  height: 100%;
+  width: 3em;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-content: center;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: 40vh;
+  flex-direction: column;
+`;
+
+const Icon = styled.img`
+  height: 3em;
+  width: 3em;
+  margin: 10px;
+`;
+
+const CopyrightText = styled.h3`
+  width: 100%;
+  margin-left: 20px;
+  writing-mode: vertical-rl;
+  transform: rotate(-180deg);
+  color: ${colors.white};
+  font-size: 12px;
+`;
+
+function LeftBar() {
+  return (
+    <nav>
+      <NavContainer>
+        <NavBar>
+          <IconContainer>
+            <Icon src={YogaIcon} />
+            <Icon src={SwimmingIcon} />
+            <Icon src={BikingIcon} />
+            <Icon src={LiftingIcon} />
+          </IconContainer>
+          <CopyrightText>Copyright SportSee 2022</CopyrightText>
+        </NavBar>
+      </NavContainer>
+    </nav>
+  );
+}
+
+export default LeftBar;
--- sportsee-front/src/pages/Dashboard.jsx
+++ sportsee-front/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Header from '../components/Header/Header';
+import LeftBar from '../components/LeftBar/LeftBar';
 
 const Home = styled.div`
   max-width: 100%;
@@ -10,6 +11,7 @@ function Dashboard() {
   return (
     <Home>
       <Header />
+      <LeftBar />
     </Home>
   );
 }
