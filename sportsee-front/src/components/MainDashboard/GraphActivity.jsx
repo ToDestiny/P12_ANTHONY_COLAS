9e3842fb40a98c86ac89a20abb0fce647a9ecd96
Anthony <anthonycolas.france@gmail.com>
2022-09-13 17:07:21 +0200

    Feature - First graph OK

--- sportsee-front/src/components/MainDashboard/GraphActivity.jsx
+++ sportsee-front/src/components/MainDashboard/GraphActivity.jsx
@@ -7,7 +7,9 @@ const Container = styled.div`
   width: 100%;
   font-weight: 500;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-between;
+
   background-color: ${colors.background};
 `;
 
@@ -25,14 +27,47 @@ const P1 = styled.p`
 const P2 = styled.p`
   color: ${colors.black};
   font-size: 10px;
+  margin: 10px;
+`;
+
+const Right = styled.div`
+  display: flex;
+  margin-top: 23px;
+  flex-direction: row;
+`;
+
+const Dot = styled.span`
+  height: 7px;
+  width: 7px;
+  background-color: #282d30;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 5px;
+`;
+
+const Dot2 = styled.span`
+  height: 7px;
+  width: 7px;
+  background-color: #e60001;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 5px;
 `;
 
 function GraphActivity() {
   return (
     <Container>
       <H1>Activité quotidienne</H1>
-      <P1>Poids(kg)</P1>
-      <P2>Calories brûlées (kCal)</P2>
+      <Right>
+        <P1>
+          <Dot />
+          Poids(kg)
+        </P1>
+        <P2>
+          <Dot2 />
+          Calories brûlées (kCal)
+        </P2>
+      </Right>
     </Container>
   );
 }
