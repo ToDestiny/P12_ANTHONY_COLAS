b070ac86ea5fb4c1c449eb03766d6822ab6f1b23
Anthony <anthonycolas.france@gmail.com>
2022-09-21 18:34:08 +0200

    Feature: Router and Error page OK

--- sportsee-front/src/index.jsx
+++ sportsee-front/src/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.scss';
 import Dashboard from './pages/Dashboard';
+import Error from './pages/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Dashboard />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/user/:userId" element={<Dashboard />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ sportsee-front/src/pages/Error.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import Header from '../components/Header/Header';
+import LeftBar from '../components/LeftBar/LeftBar';
+
+const Home = styled.div`
+  max-width: 100%;
+  overflow-x: hidden;
+`;
+
+const ErrorContainer = styled.div`
+  position: absolute;
+  height: 100vh;
+  width: 100%;
+  top: 4em;
+  left: 5rem;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ErrorTitle = styled.h2`
+  color: white;
+  font-size: 150px;
+`;
+
+const ErrorText = styled.p`
+  color: white;
+  font-size: 50px;
+`;
+
+function Error() {
+  return (
+    <Home>
+      <Header />
+      <LeftBar />
+      <ErrorContainer>
+        <ErrorTitle>404</ErrorTitle>
+        <ErrorText>Page not found</ErrorText>
+      </ErrorContainer>
+    </Home>
+  );
+}
+
+export default Error;
